64a79a0c340c6dd502f34cd60ec8856ee27bed04
hohasic <hohasic@gmail.com>
2024-02-28 15:28:03 +0900

    create project

---
+++ src/main/java/com/office/dbconntest/DbconntestApplication.java
@@ -0,0 +1,13 @@
+package com.office.dbconntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbconntestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbconntestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/office/dbconntest/DbconntestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.office.dbconntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbconntestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
