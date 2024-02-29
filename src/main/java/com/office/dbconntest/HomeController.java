914a4552f3c0947b222e8fd98ea6f6aa9067eab3
hohasic <hohasic@gmail.com>
2024-02-29 11:42:48 +0900

    ^^

--- src/main/java/com/office/dbconntest/HomeController.java
+++ src/main/java/com/office/dbconntest/HomeController.java
@@ -1,5 +1,8 @@
 package com.office.dbconntest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,6 +17,22 @@ public class HomeController {
 		log.info("home()");
 		
 		String nextPage = "home";
+		
+		try {
+			
+			Class.forName("oracle.jdbc.OracleDriver");
+			Connection conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@localhost:1521:XE", 
+					"system", 
+					"1234");
+			
+			log.info("conn: " + conn);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		
 		
 		return nextPage;
 	}
